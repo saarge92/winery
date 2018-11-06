1cdb658327fccb5c6ee858c1f91e836732a51f90
sarage92 <sarage92@mail.ru>
2018-11-06 16:20:56 +0400

    migrations issues fixed

--- database/migrations/2018_10_25_063134_create_sliders_table.php
+++ database/migrations/2018_10_25_063134_create_sliders_table.php
@@ -1,34 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateSlidersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('sliders', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('src_image');
-            $table->string('content');
-            $table->boolean('is_active')->default(true);
-            $table->timestamps();
-        });
-    }
+class CreateSlidersTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('sliders', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('src_image');
+			$table->string('content')->nullable();
+			$table->boolean('is_active')->default(true);
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('sliders');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('sliders');
+	}
 }
