4384c2e63b9732fdc1f66fd1d708c7e7256f5417
Serdar Durdyev <durdyev@megatec.ru>
2019-06-05 10:52:03 +0400

    Has any role fix

--- app/User.php
+++ app/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable
     {
         if (is_array($roles)) {
             foreach ($roles as $role) {
-                if ($this > hasRole($role)) {
+                if ($this -> hasRole($role)) {
                     return true;
                 }
             }
