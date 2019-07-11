233c77367e7e56cd86913faa157fc7fc9ef8960c
Serdar Durdyev <sarage92@mail.ru>
2019-07-11 14:17:56 +0400

    Change link to extern websites

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -2,8 +2,8 @@
 <footer class="footer py-2 bg-dark">
     <div class="container">
         <div class="m-0 text-center text-white">
-            <a href="https://vk.com/barhouse_new" class="social-icons" target="_blank"><i class="fab fa-vk"></i></a>
-            <a href="https://www.instagram.com/barhousecafe/" class="social-icons" target="_blank"><i class="fab fa-instagram"></i>
+            <a href="https://vk.com/cafecrem" class="social-icons" target="_blank"><i class="fab fa-vk"></i></a>
+            <a href="https://www.instagram.com/krem_cafe/" class="social-icons" target="_blank"><i class="fab fa-instagram"></i>
             </a>
         </div>
         <p class="m-0 text-center text-white"> Астрахань &copy; Крем cafe {{date('Y')}}</p>
