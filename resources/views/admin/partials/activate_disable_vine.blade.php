41e9ef82b18ca7d8da51b4402eae28293ec17a72
Serdar Durdyev <sarage92@mail.ru>
2019-12-06 15:06:07 +0400

    List wine display for admin

--- resources/views/admin/partials/activate_disable_vine.blade.php
+++ resources/views/admin/partials/activate_disable_vine.blade.php
@@ -1,23 +1,23 @@
-@if($vine->is_active == true)
-    <div class="mx-auto mt-2">
-        <form action="{{route('deactivateVine',['id'=>$vine->id])}}" method="POST">
-            {{csrf_field()}}
-            <button type="submit" class="btn btn-outline-danger deactivate-btn">Деактивировать</button>
-        </form>
-    </div>
-    @else
-    <div class="mx-auto mt-2">
-        <form class="" action="{{route('activateVine',['id'=>$vine->id])}}" method="post">
-            {{csrf_field()}}
-            <button type="submit" class="btn btn-outline-primary deactivate-btn">Активировать</button>
-        </form>
-    </div>
-    @endif
-    <div class="mx-auto mt-1">
-        <form action="{{route('deleteVine',['id'=>$vine->id])}}" method="POST">
-            {{csrf_field()}}
-            <button type="submit" class="btn btn-outline-dark delete">
-                <i class="fas fa-times"></i>Удалить
-            </button>
-        </form>
-    </div>
+@if($vine['is_active'] == true)
+<div class="mx-auto mt-2">
+    <form action="{{route('deactivateVine',['id'=>$vine['id']])}}" method="POST">
+        {{csrf_field()}}
+        <button type="submit" class="btn btn-outline-danger deactivate-btn">Деактивировать</button>
+    </form>
+</div>
+@else
+<div class="mx-auto mt-2">
+    <form class="" action="{{route('activateVine',['id'=>$vine['id']])}}" method="post">
+        {{csrf_field()}}
+        <button type="submit" class="btn btn-outline-primary deactivate-btn">Активировать</button>
+    </form>
+</div>
+@endif
+<div class="mx-auto mt-1">
+    <form action="{{route('deleteVine',['id'=>$vine['id']])}}" method="POST">
+        {{csrf_field()}}
+        <button type="submit" class="btn btn-outline-dark delete">
+            <i class="fas fa-times"></i>Удалить
+        </button>
+    </form>
+</div>
--- resources/views/admin/partials/listVinesAdmin.blade.php
+++ resources/views/admin/partials/listVinesAdmin.blade.php
@@ -1,27 +1,28 @@
 @foreach ($vines_for_review->chunk(3) as $vine_chink)
 <div class="row">
     @foreach($vine_chink as $vine)
-    <div class="col-md-4 col-sm-6  mt-2 {{ $vine->is_active ? '' : 'inactive' }}">
+    <div class="col-md-4 col-sm-6  mt-2 {{ $vine['is_active'] ? '' : 'inactive' }}">
         <div class="card">
             @include('admin.partials.activate_disable_vine')
             <div class="image-holder">
-                <img src="{{$vine->image_src ? Storage::url($vine->image_src) : Storage::url('projectFolders/unknow.png')}}">
+                <img
+                    src="{{$vine['image_src'] ? Storage::url($vine['image_src']) : Storage::url('projectFolders/unknow.png')}}">
                 </img>
             </div>
             <div class="description">
-                {{$vine->name_rus}},
-                {{$vine->color}} {{$vine->sweet}},
-                {{$vine->country}} {{$vine->year}} г,
+                {{$vine['name_rus']}},
+                {{$vine['color']}} {{$vine['sweet']}},
+                {{$vine['country']}} {{$vine['year']}} г,
                 <i class="fas fa-wine-bottle">
                 </i>
-                {{$vine->price}} руб,
+                {{$vine['price']}} руб,
                 <i class="fas fa-wine-glass">
                 </i>
-                {{$vine->price_cup}} руб,
-                {{$vine->volume / 1000}} л
+                {{$vine['price_cup']}} руб,
+                {{$vine['volume'] / 1000}} л
             </div>
             <div class="button-holder">
-                <a class="text-white btn btn-success" href="{{route('editVine',['id'=>$vine->id])}}">
+                <a class="text-white btn btn-success" href="{{route('editVine',['id'=>$vine['id']])}}">
                     <i class="fas fa-search-plus">
                     </i>
                     Посмотреть
