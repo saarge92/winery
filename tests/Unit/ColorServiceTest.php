255420865ed7357382944189753cb500395235da
Serdar Durdyev <sarage92@mail.ru>
2020-03-04 17:55:20 +0400

    Test edit color

--- tests/Unit/ColorServiceTest.php
+++ tests/Unit/ColorServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\color;
 use App\Interfaces\IServices\IColorService;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -20,13 +21,23 @@ class ColorServiceTest extends TestCase
     /**
      * Тестирование создания цвета в базе
      * Тестирование метода addColor
-     *
-     * @return void
      */
     public function testCreate()
     {
         $colorService = resolve(IColorService::class);
         $createdColor = $colorService->addColor(['name' => $this->faker->name]);
         $this->assertSame($createdColor, true);
+    }
+
+    /**
+     * Тестирование редактирования цвета вина в базе по id
+     * Тестирование метода editColor
+     */
+    public function testEditColor()
+    {
+        $colorService = resolve(IColorService::class);
+        $randomColor = color::orderByRaw("RAND()")->first();
+        $editedColor = $colorService->editColor(['name_color' => $this->faker->name], $randomColor['id']);
+        $this->assertSame($editedColor, true);
     }
 }
